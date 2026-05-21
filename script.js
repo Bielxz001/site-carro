f0458fa04e6c47c50b1f1f228cf625417b342def
Gabriel Coimbra <gabrielcoimbra@gmail.com>
2026-05-21 09:57:00 -0500

    Atualizado projeto

--- script.js
+++ script.js
@@ -3,41 +3,45 @@ let nextButton = document.getElementById("next");
 let container = document.querySelector(".container");
 let itens = document.querySelectorAll(".list .itens");
 let indicator = document.querySelector(".indicators");
-let dots = document.querySelectorAll("ul li");
+let dots = document.querySelectorAll(".indicators ul li");
+
 let active = 0;
 let firstPosition = 0;
 let lastPosition = itens.length - 1;
 
-nextButton.onclick = () => {
-  let itenOld = container.querySelector(".list .itens.active");
-  itenOld.classList.remove("active");
+function updateSlider() {
+  // remove item ativo
+  let itemOld = container.querySelector(".list .itens.active");
+  itemOld.classList.remove("active");
 
-  active = active + 1 > lastPosition ? 0 : active + 1;
+  // adiciona novo item ativo
   itens[active].classList.add("active");
 
+  // remove dot ativo
   let dotsOld = indicator.querySelector("ul li.active");
+
   if (dotsOld) {
     dotsOld.classList.remove("active");
   }
+
+  // adiciona dot ativo
   dots[active].classList.add("active");
+
+  // atualiza número
   indicator.querySelector(".number").innerHTML = String(active + 1).padStart(
     2,
     "0",
   );
+}
+
+// botão next
+nextButton.onclick = () => {
+  active = active + 1 > lastPosition ? 0 : active + 1;
+  updateSlider();
 };
 
+// botão prev
 prevButton.onclick = () => {
-  let itenOld = container.querySelector(".list .itens.active");
-  itenOld.classList.remove("active");
-
   active = active - 1 < firstPosition ? lastPosition : active - 1;
-  itens[active].classList.add("active");
-  let dotsOld = indicator.querySelector("ul li.active");
-if (dotsOld) {
-  dotsOld.classList.remove("active");
-}
-dots[active].classList.add("active");
-
-indicator.querySelector('.number').innerHTML =
-  String(active + 1).padStart(2, "0");
+  updateSlider();
 };
